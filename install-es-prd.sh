acfb8621a59aca75601f6707c6111c8c9fb5d8b9
huytvdev <huy8895@gmail.com>
2024-01-05 21:43:54 +0700

    update install-es-prd.sh

--- install-es-prd.sh
+++ install-es-prd.sh
@@ -2,13 +2,27 @@
 
 # Cập nhật 12/2019
 
-# Tat Firewall
-systemctl disable firewalld >/dev/null 2>&1
-systemctl stop firewalld
+sudo yum update -y
+sudo yum install telnet -y
+sudo yum install bind-utils -y
+sudo yum install net-tools -y
+sudo yum install bash-completion bash-completion-extras
+source /etc/profile.d/bash_completion.sh
+
+#Disable SELinux
+sudo setenforce 0
+sudo sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+#Disable firewall
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+
+#Enable IP Forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
 
 # Tat swap
-sed -i '/swap/d' /etc/fstab
-swapoff -a
+sudo sed -i '/swap/d' /etc/fstab
+sudo swapoff -a
 
 echo "--->>> start install java"
 sudo yum -y update
